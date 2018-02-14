cbb987f690d149cf2a48a02ec2c3272822da2e0b
janngomaa <sowdms@gmail.com>
2018-02-14 02:00:44 +0000

    Updates on gitconfig.sh

--- gitconfig.sh
+++ gitconfig.sh
@@ -1,23 +1,22 @@
-# Option 1: Initializing ==> Uncomment untill line before Option 2
-#https://www.howtoforge.com/tutorial/install-git-and-github-on-ubuntu-14.04/
-#https://git-scm.com/book/fr/v1/Les-bases-de-Git-Travailler-avec-des-d%C3%A9p%C3%B4ts-distants
-#  Run in Docker container
-#  docker exec -i -t sparkc sh work/shud/gitconfig.sh
+#  Option 1: Initializing ==> Uncomment untill line before Option 2
+#  https://www.howtoforge.com/tutorial/install-git-and-github-on-ubuntu-14.04/
+#  https://git-scm.com/book/fr/v1/Les-bases-de-Git-Travailler-avec-des-d%C3%A9p%C3%B4ts-distants
+#  Command to run in Docker container: docker exec -i -t sparkc sh work/shud/gitconfig.sh
 
-#Initializing git on
+#  Initializing git on a local repo (for the first time)
 #cd work
 #echo "*****  Initializing git on $PWD/shud  *****" 
 #git init shud
 #cd shud
 #git remote add origin https://github.com/janngomaa/shud.git
 
-#Option 2: Cloning existing repo
+#  Option 2: Cloning existing repo
 
 #git clone https://github.com/janngomaa/shud
 
-echo "*****  Adding all element in $PWD to git repo  *****" 
-
 cd work/shud
+
+echo "*****  Adding all element in $PWD to git repo  *****" 
 
 git pull origin master
 
