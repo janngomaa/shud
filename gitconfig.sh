c19c9043d0e590adff320c42ff4599550052e730
janngomaa <sowdms@gmail.com>
2018-02-14 00:15:03 +0000

    Commiting changes on gitconfig

--- gitconfig.sh
+++ gitconfig.sh
@@ -1,19 +1,19 @@
 #  Run Scrapy from Docker 
 #  docker exec -i -t sparkc sh work/shud/gitconfig.sh
 
-cd work
-
+#Initializing git on
+#cd work
 #echo "*****  Initializing git on $PWD/shud  *****" 
 #git init shud
+#cd shud
+#git remote add origin https://github.com/janngomaa/shud.git
 
-cd shud
 echo "*****  Adding all element in $PWD to git repo  *****" 
+
+cd work/shud
+
 git add *
 
 git commit -m "$1"
-
-#git remote add origin https://github.com/janngomaa/shud.git
-
-#git pull remote master
 
 git push origin master
