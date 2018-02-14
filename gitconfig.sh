d711f2ed9cb5e43c0ed2c513f160585ea902628f
janngomaa <sowdms@gmail.com>
2018-02-14 00:41:06 +0000

    Commiting changes on gitconfig

--- gitconfig.sh
+++ gitconfig.sh
@@ -1,4 +1,6 @@
-#  Run Scrapy from Docker 
+# Option 1: Initializing ==> Uncomment untill line before Option 2
+#https://www.howtoforge.com/tutorial/install-git-and-github-on-ubuntu-14.04/
+#  Run in Docker container
 #  docker exec -i -t sparkc sh work/shud/gitconfig.sh
 
 #Initializing git on
@@ -7,6 +9,10 @@
 #git init shud
 #cd shud
 #git remote add origin https://github.com/janngomaa/shud.git
+
+#Option 2: Cloning existing repo
+
+#git clone https://github.com/janngomaa/shud
 
 echo "*****  Adding all element in $PWD to git repo  *****" 
 
